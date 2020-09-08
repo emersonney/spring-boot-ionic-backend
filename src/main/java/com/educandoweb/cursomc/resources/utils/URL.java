9d6c856c45261846d2e7693cdb2be3ff45fc618f
emersonney <emerson.farias@gmail.com>
2020-09-08 14:51:05 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/educandoweb/cursomc/resources/utils/URL.java
@@ -0,0 +1,43 @@
+package com.educandoweb.cursomc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+//import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class URL {
+
+	public static String decodeParam(String s) {
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+	
+	
+	public static List<Integer> decodeIntList(String s){
+		
+/*
+  		String[] vet = s.split(",");
+		List<Integer> list = new ArrayList<>();
+		
+		for( int i=0; i < vet.length ; i++ ) {
+			
+			list.add(Integer.parseInt(vet[i]));
+			
+		}
+		
+		return list;
+*/
+		//O mesmo codigo usado a cima, mas feito em uma unica linha utilizando expressão Lambda
+		return Arrays.asList(s.split(","))
+				.stream().map(x -> Integer.parseInt(x))
+				.collect(Collectors.toList());		
+	}
+	
+	
+	
+}
