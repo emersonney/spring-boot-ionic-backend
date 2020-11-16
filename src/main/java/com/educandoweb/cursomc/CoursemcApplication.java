e526e32baf47b9bda50ba8dc8e5ea7c4c1994b11
emersonney <emerson.farias@gmail.com>
2020-11-16 16:53:35 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/educandoweb/cursomc/CoursemcApplication.java
+++ src/main/java/com/educandoweb/cursomc/CoursemcApplication.java
@@ -1,18 +1,27 @@
 package com.educandoweb.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.educandoweb.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CoursemcApplication implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoursemcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		s3Service.uploadFile("C:\\temp\\fotos\\computador.jpg");
+		
 	}	
 	
 }
